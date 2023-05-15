bea197e77a8c6ddb0b4c691995c74ea2a62f10bd
geavenx <vitordonnangeloc@gmail.com>
2023-05-15 12:54:35 -0300

    formatting

--- golang/web-dev/news/main.go
+++ golang/web-dev/news/main.go
@@ -1,128 +1,128 @@
 package main
 
 import (
-  "net/http"
-  "os"
-  "log"
-  "html/template"
-  "net/url"
-  "strconv"
-  "time"
-  "bytes"
-  "math"
+	"bytes"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
 
-  "github.com/geavenx/newsApi"
-  "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
+
+	"github.com/geavenx/newsApi"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
 type Search struct {
-  Query      string
-  NextPage   int
-  TotalPages int
-  Results    *newsApi.Results
+	Query      string
+	NextPage   int
+	TotalPages int
+	Results    *newsApi.Results
 }
 
 func (s *Search) IsLastPage() bool {
-        return s.NextPage >= s.TotalPages
+	return s.NextPage >= s.TotalPages
 }
 
 func (s *Search) CurrentPage() int {
-        if s.NextPage == 1 {
-          return s.NextPage
-        }
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
 
-        return s.NextPage -1
+	return s.NextPage - 1
 }
 
 func (s *Search) PreviousPage() int {
-        return s.CurrentPage() - 1
-  }
+	return s.CurrentPage() - 1
+}
 
-//the {w} parameter is the structure we use to send responses to an HTTP request.
+// the {w} parameter is the structure we use to send responses to an HTTP request.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  tpl.Execute(w, nil)
+	tpl.Execute(w, nil)
 }
 
 func searchHandler(newsapi *newsApi.Client) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    u, err := url.Parse(r.URL.String())
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-  }
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-  params := u.Query()
-  searchQuery := params.Get("q")
-  page := params.Get("page")
-  if page == "" {
-    page = "1"
-  }
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
 
-  results, err := newsapi.FetchEverything(searchQuery, page)
-  if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-  nextPage, err := strconv.Atoi(page)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-  }
-  
-    search := &Search{
-      Query: searchQuery,
-      NextPage: nextPage,
-      TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
-      Results: results,
-  }
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-    if ok := !search.IsLastPage(); ok {
-      search.NextPage++
-    }
+		search := &Search{
+			Query:      searchQuery,
+			NextPage:   nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results:    results,
+		}
 
-  buf := &bytes.Buffer{}
-  err = tpl.Execute(buf, search)
-  if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-  }
+		if ok := !search.IsLastPage(); ok {
+			search.NextPage++
+		}
 
-    buf.WriteTo(w)
-  }
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+	}
 }
 
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Println("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
-  
-  apikey := os.Getenv("NEWS_API_KEY")
-  if apikey == "" {
-    log.Fatal("Env: apikey must be set")
-  }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  myClient := &http.Client{Timeout: 10 * time.Second}
-  newsapi := newsApi.NewClient(myClient, apikey, 20)
+	apikey := os.Getenv("NEWS_API_KEY")
+	if apikey == "" {
+		log.Fatal("Env: apikey must be set")
+	}
 
-  fs := http.FileServer(http.Dir("assets"))
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := newsApi.NewClient(myClient, apikey, 20)
 
-  //this method is used to create an HTTP request multiplexer
-  mux := http.NewServeMux()   
-  mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-  
-  mux.HandleFunc("/", indexHandler)
-    mux.HandleFunc("/search", searchHandler(newsapi))
+	fs := http.FileServer(http.Dir("assets"))
 
-  //{http.ListenAndServe()} method which starts the server on the port defined in the port variable.
-  http.ListenAndServe(":"+port, mux)
+	// this method is used to create an HTTP request multiplexer
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler(newsapi))
+
+	//{http.ListenAndServe()} method which starts the server on the port defined in the port variable.
+	http.ListenAndServe(":"+port, mux)
 }
-
