0bc6ba4a285e87ebe00b756858d9f718ee7e2231
geavenx <vitordonnangeloc@gmail.com>
2023-05-15 12:55:51 -0300

    formatting newsApi

--- golang/web-dev/news/newsApi/init.go
+++ golang/web-dev/news/newsApi/init.go
@@ -1,75 +1,79 @@
 package newsApi
 
 import (
-  "net/http"
-  "time"
-  "fmt"
-  "encoding/json"
-  "io/ioutil"
-  "net/url"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 )
 
 type Article struct {
+	Source struct {
+		ID   interface{} `json: "id"`
+		Name string      `json: "name"`
+	} `json: "source"`
 
-  Source struct {
-    ID interface{} `json: "id"`
-    Name string `json: "name"`
-  } `json: "source"`
-  
-  Author string `json: "author"`
-  Title string `json: "title"`
-  Description string `json: "description"`
-  URL string `json: "URL"`
-  URLToImage string `json: "urltoimage"`
-  PublishedAt time.Time `json: "publishedAt"`
-  Content string `json: "content"`
+	Author      string    `json: "author"`
+	Title       string    `json: "title"`
+	Description string    `json: "description"`
+	URL         string    `json: "URL"`
+	URLToImage  string    `json: "urltoimage"`
+	PublishedAt time.Time `json: "publishedAt"`
+	Content     string    `json: "content"`
 }
 
 type Results struct {
-  Status string `json: "status"`
-  TotalResults int `json: "totalResults"`
-  Articles []Article `json: "articles"`
+	Status       string    `json: "status"`
+	TotalResults int       `json: "totalResults"`
+	Articles     []Article `json: "articles"`
 }
 
 type Client struct {
-  http *http.Client
-  key string
-  PageSize int
+	http     *http.Client
+	key      string
+	PageSize int
 }
 
 func (a *Article) FormatPublishedDate() string {
-  year, month, day := a.PublishedAt.Date()
-  return fmt.Sprintf("%v %d, %d", month, day, year)
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v %d, %d", month, day, year)
 }
 
 func (c *Client) FetchEverything(query, page string) (*Results, error) {
-  endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
-  
-  resp, err := c.http.Get(endpoint)
-  if err != nil {
-    return nil, err
-  }
+	endpoint := fmt.Sprintf(
+		"https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en",
+		url.QueryEscape(query),
+		c.PageSize,
+		page,
+		c.key,
+	)
 
-  defer resp.Body.Close()
-  
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return nil, err
-  }
+	resp, err := c.http.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
 
-  if resp.StatusCode != http.StatusOK {
-    return nil, fmt.Errorf(string(body))
-  }
+	defer resp.Body.Close()
 
-  res := &Results{}
-  return res, json.Unmarshal(body, res)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &Results{}
+	return res, json.Unmarshal(body, res)
 }
 
-func NewClient(httpClient *http.Client, key string, pageSize int) * Client {
-  if pageSize > 100 {
-    pageSize = 100
-  }
+func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
+	if pageSize > 100 {
+		pageSize = 100
+	}
 
-  return &Client{httpClient, key, pageSize}
+	return &Client{httpClient, key, pageSize}
 }
-
