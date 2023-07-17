268b3a5a6f547fb4dc5f218a70ef2dab5185fc91
Vitor Cardoso <vcardoso@SECNTL-6GC1BB3>
2023-07-16 21:20:13 -0300

    JSON output added

--- golang/web-scraper/scraper.go
+++ golang/web-scraper/scraper.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -11,23 +12,25 @@ import (
 )
 
 type PokemonProduct struct {
-	url   string
-	image string
-	name  string
-	price string
+	URL   string `json:"url"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+	Image string `json:"image"`
 }
 
 func main() {
 	url := flag.String("url", "https://scrapeme.live/shop/page/1/", "URL to scrap.")
-	selector := flag.String("selector", "li.product", "custom CSS selectors.")
+	selector := flag.String("selector", "a[href]", "custom CSS selectors.")
 	allPages := flag.Bool("all", false, "Follow all pages of products.")
+	/* 	depth := flag.Int("depth", 1, "Depth of the scrapping.")
+	 */format := flag.String("format", "csv", "Select the output format (CSV or JSON).")
 
 	flag.Parse()
 
-	scrap(*url, *selector, *allPages)
+	scrap(*url, *selector, *format, *allPages)
 }
 
-func scrap(url, selector string, follow bool) {
+func scrap(url, selector, format string, follow bool) {
 	var pokemonProducts []PokemonProduct
 	var visitedUrls []string
 
@@ -49,28 +52,26 @@ func scrap(url, selector string, follow bool) {
 		fmt.Println("Finished", r.Request.URL)
 	})
 
-	c.OnHTML(selector, func(e *colly.HTMLElement) {
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
 		pokemonProduct := PokemonProduct{}
 
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
+		pokemonProduct.URL = e.ChildAttr("a", "href")
+		pokemonProduct.Image = e.ChildAttr("img", "src")
+		pokemonProduct.Name = e.ChildText("h2")
+		pokemonProduct.Price = e.ChildText(".price")
 
 		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
 
 	if follow {
-		c.OnHTML("a.page-numbers[href]", func(e *colly.HTMLElement) {
+		c.OnHTML(selector, func(e *colly.HTMLElement) {
 			link := e.Attr("href")
 			fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 
 			for _, address := range visitedUrls {
 				if link != address {
 					c.Visit(link)
-				} /* else {
-					fmt.Println("Link already scrapped, ignoring...")
-				} */
+				}
 			}
 
 			visitedUrls = append(visitedUrls, link)
@@ -79,9 +80,26 @@ func scrap(url, selector string, follow bool) {
 
 	c.Visit(url)
 
+	switch format {
+	case "csv", "CSV":
+		if err := writeCSV(pokemonProducts); err != nil {
+			log.Fatalln("Failed to create CSV", err)
+		}
+		fmt.Println("products.csv created!")
+	case "JSON", "json":
+		if err := writeJSON(pokemonProducts); err != nil {
+			log.Fatalln("Failed to create JSON", err)
+		}
+		fmt.Println("products.json created!")
+	default:
+		fmt.Println("Invalid format!!")
+	}
+}
+
+func writeCSV(pokemonProducts []PokemonProduct) error {
 	file, err := os.Create("products.csv")
 	if err != nil {
-		log.Fatalln("Failed to create csv", err)
+		return err
 	}
 	defer file.Close()
 
@@ -98,13 +116,32 @@ func scrap(url, selector string, follow bool) {
 
 	for _, pokemonProduct := range pokemonProducts {
 		record := []string{
-			pokemonProduct.url,
-			pokemonProduct.image,
-			pokemonProduct.name,
-			pokemonProduct.price,
+			pokemonProduct.URL,
+			pokemonProduct.Image,
+			pokemonProduct.Name,
+			pokemonProduct.Price,
 		}
 
 		writer.Write(record)
 		defer writer.Flush()
+
 	}
+	return nil
+}
+
+func writeJSON(pokemonProducts []PokemonProduct) error {
+	file, err := os.Create("products.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := json.NewEncoder(file)
+	writer.SetIndent("", "  ")
+
+	if err := writer.Encode(pokemonProducts); err != nil {
+		return err
+	}
+
+	return nil
 }
